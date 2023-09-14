26cf9fdb6e15857bf545fbe3fabe611548c77259
kchuang1 <kchuang1@nuvoton.com>
2023-09-14 14:58:47 +0800

    Update sample code for debug port changed to Uart1 PA.8/PA.9
    Change-Id: I53fdda14e794730a0dd5566e66a94c438aa05c30

--- SampleCode/StdDriver/LPI2C_AutoOperation/main.c
+++ SampleCode/StdDriver/LPI2C_AutoOperation/main.c
@@ -323,7 +323,7 @@ void SYS_Init(void)
     CLK_EnableModuleClock(HCLK1_MODULE);
 
     /* Enable UART clock */
-    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_EnableModuleClock(UART1_MODULE);
 
     /* Enable LPI2C0 clock */
     CLK_EnableModuleClock(LPI2C0_MODULE);
@@ -341,14 +341,14 @@ void SYS_Init(void)
     CLK_EnableModuleClock(GPA_MODULE);
 
     /* Select UART clock source from HIRC */
-    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL4_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+    CLK_SetModuleClock(UART1_MODULE, CLK_CLKSEL4_UART1SEL_HIRC, CLK_CLKDIV0_UART1(1));
 
     /* Update System Core Clock */
     /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock and cyclesPerUs automatically. */
     SystemCoreClockUpdate();
 
-    /* Set PB multi-function pins for UART0 RXD=PB.12 and TXD=PB.13 */
-    Uart0DefaultMPF();
+    /* Set GPA multi-function pins for UART1 RXD(PA.8) and TXD(PA.9) */
+    Uart1DefaultMPF();
 
     /* Set LPI2C0 multi-function pins */
     SYS->GPA_MFP1 = (SYS->GPA_MFP1 & ~(SYS_GPA_MFP1_PA4MFP_Msk | SYS_GPA_MFP1_PA5MFP_Msk)) |
@@ -484,7 +484,7 @@ int32_t main (void)
     SYS_Init();
 
     /* Init UART to 115200-8n1 for print message */
-    UART_Open(UART0, 115200);
+    UART_Open(UART1, 115200);
 
     /*--------------------------------------------------------------------------------------------------------------*/
     /* Autmatic Operation Mode Test                                                                                 */
